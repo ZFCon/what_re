9f39392be6a05e5d2455a86c286223308cf33e47
Ziad Ahmed <zaid.ahmed@quickcheck.ng>
2023-04-30 22:54:25 +0300

    finish styling

--- frontend/src/components/ProductList.jsx
+++ frontend/src/components/ProductList.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Table, Button } from 'react-bootstrap';
+import { Container, Table, Button, Navbar, Form, FormControl } from 'react-bootstrap';
 
 function ProductList(props) {
   const products = [
@@ -15,6 +15,21 @@ function ProductList(props) {
   return (
     <Container className="my-5">
       <h1>Product List</h1>
+
+      <Navbar bg="light" expand="lg">
+        <Container className='justify-content-end'>
+          <Form className="d-flex">
+            <FormControl
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Container>
+      </Navbar>
+
       <Table striped bordered hover>
         <thead>
           <tr>
