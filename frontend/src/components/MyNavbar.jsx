3a8171110722a3a422ca3a40b047e803ce942d0b
Ziad Ahmed <zaid.ahmed@quickcheck.ng>
2023-04-30 22:34:33 +0300

    change button place

--- frontend/src/components/MyNavbar.jsx
+++ frontend/src/components/MyNavbar.jsx
@@ -8,15 +8,17 @@ export default function MyNavbar() {
             <div className='container d-flex'>
                 <Navbar.Brand href="#">What Digital</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav>
-                        <NavDropdown title={<Image src={avatar} roundedCircle width="50" />} id="basic-nav-dropdown">
-                            <NavDropdown.Item disabled>Ziad</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#">Logout</NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav>
-                </Navbar.Collapse>
+                <div alignRight>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav>
+                            <NavDropdown title={<Image src={avatar} roundedCircle width="50" />} id="basic-nav-dropdown">
+                                <NavDropdown.Item disabled>Ziad</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#">Logout</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                </div>
             </div>
         </Navbar>
     );
