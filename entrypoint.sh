1914e6f4c4586050e3fa75e0dc3930e6f36393ec
Ziad Ahmed <zaid.ahmed@quickcheck.ng>
2023-05-04 16:04:53 +0300

    test fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
+# Wait for the PostgreSQL database to be ready
+if [ "$DATABASE" = "$POSTGRES_DB" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z db $POSTGRES_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+# Apply database migrations
 python manage.py migrate
 
 exec "$@"
