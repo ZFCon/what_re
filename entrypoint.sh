39aeeb91a3301020994a3cd0009cab3b699ca542
Ziad Ahmed <zaid.ahmed@quickcheck.ng>
2023-05-04 16:25:01 +0300

    run

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-set -e
-
-if [ "${DIVIO_DB_WAIT}" = "1" ]; then
-    /usr/local/bin/divio-wait-postgres.sh
-fi
-
 # Run any database migrations
 python manage.py migrate
+
+# Run Project
+uwsgi --http=0.0.0.0:80 --module=what_re.wsgi
 
 exec "$@"
